160072745bc54ae7c5ea233dc0d3ccd6492a1e3e
Fares Yasser <113234989+fares1583@users.noreply.github.com>
2024-08-21 17:13:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let input = document.querySelector("input"),
+  btn = document.querySelector(".btn"),
+  showData = document.querySelector(".show-data");
+
+btn.onclick = function () {
+  getRepos();
+};
+
+function getRepos() {
+  if (input.value == "") {
+    showData.innerHTML = "<span> Please Write a GitHub Username</span> ";
+  } else {
+    fetch(`https://api.github.com/users/${input.value}/repos`)
+      .then((res) => res.json())
+      .then((repos) => {
+        showData.innerHTML = "";
+
+        repos.forEach((repo, index) => {
+          console.log(repo);
+          let repoName = document.createElement("div");
+          // repoName.innerHTML = `${index + 1} - ${repo.name}`;
+          showData.appendChild(repoName);
+
+          let span = document.createElement("span");
+          span.innerHTML = `${index + 1} - ${repo.name}`;
+          span.style.width = "200px";
+          repoName.appendChild(span);
+
+          let urlName = document.createElement("a");
+          urlName.appendChild(document.createTextNode("Visit Site If Exist"));
+          urlName.setAttribute("target", "_blank");
+          urlName.href = ` https://${input.value}.github.io/${repo.name}/ `;
+          repoName.appendChild(urlName);
+
+          let visitRepo = document.createElement("a");
+          visitRepo.appendChild(document.createTextNode("Visit The repo"));
+          visitRepo.setAttribute("target", "_blank");
+          visitRepo.href = ` https:/github.com/${input.value}/${repo.name} `;
+          repoName.appendChild(visitRepo);
+        });
+        console.log(repos.length);
+      });
+  }
+}
